7b87cd9003e12a9aaedf8a2b1d2efdbaaec5b22a
Kufel <kufixen@gmail.com>
2018-01-21 21:58:50 +0100

    Pierwszy commit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+class Samochody
+{
+public:
+    void start()
+    {
+    cout<<"Witam. Wywolane metoda start. "<<endl;
+    }
+};
+
 int main()
 {
+    Samochody s1;
     cout << "Hello world!" << endl;
-
+    s1.start();
     return 0;
 }
