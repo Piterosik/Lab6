5d82eebbd496c0c5e3f45baff7f130db763d8558
Kucyponek <35694114+Kucyponek@users.noreply.github.com>
2018-01-22 17:29:17 +0100

    Add files via upload
    ok jeszcze tylko Start()

--- src/Druzyna.cpp
+++ src/Druzyna.cpp
@@ -20,11 +20,25 @@ void Druzyna::policzWynik()
     _wynik=0
     for (int i=0; i<3; i++)
     {
-        if (_wynikiWlasne[i]>_wynikiPrzeciwników[i])
+        if (_wynikiWlasne[i]>_wynikiPrzeciwnikow[i])
             _wynik+=3;
-        else if (_wynikiWlasne[i]==_wynikiPrzeciwników[i])
+        else if (_wynikiWlasne[i]==_wynikiPrzeciwnikow[i])
             _wynik+=1;
         else
             _wynik+=0;
+    }
+}
+
+void Druzyna::podajWynikiMeczów()
+{
+    for (int i=0; i<3; i++)
+    {
+        cout << "Podaj wyniki " << i+1 << ". meczu!" << endl;
+        cout << "Wynik druzyny: ";
+        cin >> _wynikiW³asne[i];
+        cout << endl;
+        cout << "Wynik przeciwnikow: ";
+        cin >> _wynikiPrzeciwnik[i];
+        cout << endl;
     }
 }
