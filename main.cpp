8cac365b294a9f867d1a2a2e0d2958fa8a360e76
Kufel <kufixen@gmail.com>
2018-01-22 17:03:49 +0100

    Funkcja zapisujaca dane

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,12 +10,44 @@ public:
     {
     cout<<"Witam. Wywolane metoda start. "<<endl;
     }
+    Samochody();
+    ~Samochody();
+    void save(string marka,string model,int rok, float pojemnosc);
+private:
+    string marka,model;
+    int rok;
+	float pojemnosc;
+
 };
+
+    Samochody::Samochody()
+    {
+
+    }
+    Samochody::~Samochody()
+    {
+
+    }
+
+   void Samochody::save(string mar,string mod,int ro,float poj)
+    {
+        marka=mar;
+        model=mod;
+        rok = ro;
+        pojemnosc = poj;
+    }
 
 int main()
 {
+    string mar, mod;
+	int ro;
+	float poj;
     Samochody s1;
+    Samochody s2;
     cout << "Hello world!" << endl;
     s1.start();
+    s2.save(mar,mod,ro,poj);
+
+
     return 0;
 }
